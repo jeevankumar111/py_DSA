62922a6358ec433251ca8a785d7863cc6049018d
vanum jeevan jyothi kumar <92968746+jeevankumar111@users.noreply.github.com>
2023-09-26 15:54:32 +0530

    Add files via upload

---
+++ 5_ChoclateDistribution.py
@@ -0,0 +1,21 @@
+def findMinDiff(arr,n,m):
+    if(m ==0 or n ==0):
+        return 0
+
+    arr.sort()
+
+    if(n<m):
+        return -1
+
+    min_diff = arr[n-1] - arr[0]
+
+    for i in range(len(arr) - m+1):
+        min_diff = min(min_diff,arr[i+m-1] - arr[i])
+
+    return min_diff
+
+if __name__ =="-__main__":
+    arr=[7,3,2,4,9,12,56]
+    m=3
+    n=len(arr)
+    print("Minimum differnce is",findMinDiff(arr,n,m))
