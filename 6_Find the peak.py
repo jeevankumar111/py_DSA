3c6b5841e37c8b4846be71f21672b4c7588eca74
vanum jeevan jyothi kumar <92968746+jeevankumar111@users.noreply.github.com>
2023-09-26 15:54:56 +0530

    Add files via upload

---
+++ 6_Find the peak.py
@@ -0,0 +1,16 @@
+def Peak_Element(arr,n):
+    if(n==1):
+        return 0
+    if(arr[0]>=arr[1]):
+        return 0
+    if(arr[n-1]>=arr[n-2]):
+        return  n-1
+
+    for i in range(1,n-1):
+        if(arr[i]>=arr[i-1]) and arr[i] >= arr[i+1]:
+            return i
+
+
+arr = [1,4,2,6,7,5,9,0]
+n=len(arr)
+print("Index of a peak point is : ",Peak_Element(arr,n))
