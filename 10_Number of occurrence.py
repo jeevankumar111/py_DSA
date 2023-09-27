07cb8d3b9bd758ef3d6cce76f0f3effeed6f7ca2
vanum jeevan jyothi kumar <92968746+jeevankumar111@users.noreply.github.com>
2023-09-27 16:44:58 +0530

    Add files via upload

---
+++ 10_Number of occurrence.py
@@ -0,0 +1,17 @@
+def countOccurence(arr,n,x):
+    count=0
+    for i in range(n):
+        if x == arr[i]:
+            count +=1
+    return count
+
+
+
+
+arr =[1,22,44,66,88,8,8]
+n=len(arr)
+x =8
+
+print("The number of Occurrences are : ",countOccurence(arr,n,x))
+
+
