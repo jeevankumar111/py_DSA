1a222b67850360d1bc8b1d1bd242d65666ea5793
vanum jeevan jyothi kumar <92968746+jeevankumar111@users.noreply.github.com>
2023-10-21 20:29:42 +0530

    Add files via upload

---
+++ 35_triplet the sum given below.py
@@ -0,0 +1,15 @@
+def findnumbers(arr,n,sum):
+    for i in range(0,n-2):
+        for j in range(i+1,n-1):
+            for k in range(j+1,n):
+                if arr[i]+arr[j]+arr[k] == sum:
+                    print("TRIPLET is",arr[i],
+                          ",",arr[j],",",arr[k])
+                    return True
+
+    return False
+
+arr =[1,4,45,6,10,8]
+sum =22
+n = len(arr)
+findnumbers(arr,n,sum)
