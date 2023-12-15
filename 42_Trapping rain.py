7f1a80e2ad387bf4cfc4454b37f88a597e4d5593
vanum jeevan jyothi kumar <92968746+jeevankumar111@users.noreply.github.com>
2023-12-15 10:39:13 +0530

    Add files via upload

---
+++ 42_Trapping rain.py
@@ -0,0 +1,23 @@
+class Solution:
+    def trappingWater(self, arr, n):
+        # Code here
+        low = 0
+        high = n - 1
+        ml = 0
+        mr = 0
+        ans = 0
+        while (low <= high):
+            if arr[low] < arr[high]:
+                if arr[low] > ml:
+                    ml = arr[low]
+                else:
+                    ans += ml - arr[low]
+                low += 1
+
+            else:
+                if arr[high] > mr:
+                    mr = arr[high]
+                else:
+                    ans += mr - arr[high]
+                high -= 1
+        return ans
