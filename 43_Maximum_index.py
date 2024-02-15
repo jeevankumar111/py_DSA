1ba5d5338401e2d7ed25469d17c6c418c677dc0b
vanum jeevan jyothi kumar <92968746+jeevankumar111@users.noreply.github.com>
2024-02-15 21:41:50 +0530

    Add files via upload

---
+++ 43_Maximum_index.py
@@ -0,0 +1,25 @@
+class Solution:
+
+	def maxIndexDiff(self,arr,n):
+	    max_diff = 0
+        left_min = [0] * n
+        right_max = [0] * n
+
+        left_min[0] = arr[0]
+        for i in range(1, n):
+            left_min[i] = min(arr[i], left_min[i - 1])
+
+        right_max[n - 1] = arr[n - 1]
+        for i in range(n - 2, -1, -1):
+            right_max[i] = max(arr[i], right_max[i + 1])
+
+        i = 0
+        j = 0
+        while i < n and j < n:
+            if left_min[i] <= right_max[j]:
+                max_diff = max(max_diff, j - i)
+                j += 1
+            else:
+                i += 1
+
+        return max_diff
