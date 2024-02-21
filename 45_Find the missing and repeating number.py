ddcf5a27038c5356640f549923852b427f0f3c12
vanum jeevan jyothi kumar <92968746+jeevankumar111@users.noreply.github.com>
2024-02-21 21:27:28 +0530

    Add files via upload

---
+++ 45_Find the missing and repeating number.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findTwoElement(self, arr, n):
+        # code here
+        sumarr = sum(arr)
+        exsum = (n * (n + 1)) / 2
+
+        for j in range(n):
+            if (arr[abs(arr[j]) - 1] < 0):
+                rep = abs(arr[j])
+                break
+            else:
+                arr[abs(arr[j]) - 1] = arr[abs(arr[j]) - 1] * (-1)
+
+        sumarr = sumarr - rep
+        miss = int(exsum - sumarr)
+
+        return [rep, miss]
