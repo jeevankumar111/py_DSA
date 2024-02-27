75516652b93c81cd9467fb15b01be2d499c86912
vanum jeevan jyothi kumar <92968746+jeevankumar111@users.noreply.github.com>
2024-02-27 22:17:05 +0530

    Add files via upload

---
+++ 48_partition subset.py
@@ -0,0 +1,16 @@
+class Solution:
+    def equalPartition(self, N, arr):
+        # code here
+        nums=arr
+        total_sum = sum(nums)
+        if total_sum % 2 != 0:
+            return False
+        target_sum = total_sum // 2
+        n = len(nums)
+        dp = [False] * (target_sum + 1)
+        dp[0] = True
+        for num in nums:
+            for i in range(target_sum, num - 1, -1):
+                dp[i] = dp[i] or dp[i - num]
+
+        return dp[target_sum]
